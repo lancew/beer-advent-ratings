a171a09815f85c68f7dadc4af0a80687cf1f1548
lancew <lw@judocoach.com>
2022-12-04 21:57:08 +0000

    Gin test 1

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # beer-advent-ratings
 Exploratory project, rating all the beers in my beer advent calendar
+
+Just a place to experiment.
+Plan is Roc for backend, Elm for the front.
+
+
---
+++ main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var db = make(map[string]string)
+
+func setupRouter() *gin.Engine {
+	// Disable Console Color
+	// gin.DisableConsoleColor()
+	r := gin.Default()
+
+	r.StaticFile("/favicon.ico", "./favicon.ico")
+	r.LoadHTMLGlob("templates/*.html")
+
+	// Ping test
+	r.GET("/ping", func(c *gin.Context) {
+		c.String(http.StatusOK, "pong")
+	})
+
+	r.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	return r
+}
+
+func main() {
+	r := setupRouter()
+	// Listen and Server in 0.0.0.0:8080
+	r.Run(":8080")
+}
