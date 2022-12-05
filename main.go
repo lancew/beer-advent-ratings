03436911109d06d17fd0df27e6955f44129bf3ec
lancew <lw@judocoach.com>
2022-12-05 21:46:12 +0000

    Read the JSON and serve it

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
 )
 
 var db = make(map[string]string)
+
+type Beer struct {
+	Alcohol_percentage float32 `json:"alcohol_percentage"`
+	Date               string  `json:"date"`
+	Name               string  `json:"name"`
+	Notes              string  `json:"notes"`
+	Rating             int     `json:"rating"`
+	Beer_type          string  `json:"beer_type"`
+}
 
 func setupRouter() *gin.Engine {
 	// Disable Console Color
@@ -23,6 +35,23 @@ func setupRouter() *gin.Engine {
 
 	r.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", nil)
+	})
+
+	r.GET("/beers", func(c *gin.Context) {
+
+		content, err := ioutil.ReadFile("./beers.json")
+		if err != nil {
+			log.Fatal("Error when opening file: ", err)
+		}
+
+		var beers []Beer
+		err = json.Unmarshal(content, &beers)
+		if err != nil {
+			log.Fatal("Error during Unmarshal(): ", err)
+		}
+
+		c.JSON(http.StatusOK, beers)
+
 	})
 
 	return r
